e92db21aa8e31535d7c42f2ba21a5fdb114176da
fgyan94 <fgyan94@gmail.com>
2021-08-16 22:15:50 -0300

    251. GameInfo: Criando estruturas e testes

--- src/templates/Base/styles.ts
+++ src/templates/Base/styles.ts
@@ -10,8 +10,8 @@ export const SectionFooter = styled.section`
     clip-path: polygon(0 5%, 100% 0%, 100% 100%, 0 100%);
 
     ${media.greaterThan('medium')`
-    padding-top: calc(${theme.spacings.xxlarge} * 2);
-    clip-path: polygon(0 15%, 100% 0%, 100% 100%, 0 100%);
-  `}
+      padding-top: calc(${theme.spacings.xxlarge} * 2);
+      clip-path: polygon(0 15%, 100% 0%, 100% 100%, 0 100%);
+    `}
   `}
 `
