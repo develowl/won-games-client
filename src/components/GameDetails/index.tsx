36e27ec42e9d51edbebc0545b4f2034c11d7eac7
fgyan94 <fgyan94@gmail.com>
2022-01-04 22:32:39 -0300

    Updating...

---
+++ src/components/GameDetails/index.tsx
@@ -0,0 +1,15 @@
+import Heading from 'components/Heading'
+import MediaMatch from 'components/MediaMatch'
+import * as S from './styles'
+
+const GameDetails = () => (
+  <S.Wrapper>
+    <MediaMatch greaterThan={'small'}>
+      <Heading lineLeft lineColor={'secondary'}>
+        Game Details
+      </Heading>
+    </MediaMatch>
+  </S.Wrapper>
+)
+
+export default GameDetails
