cebf168672c470f6381804fcd05ac98bfcf3acf1
fgyan94 <fgyan94@gmail.com>
2022-01-20 23:14:13 -0300

    Updating...

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, Story } from '@storybook/react/types-6-0'
-import GameDetails from '.'
+import GameDetails, { GameDetailsProps } from '.'
 
 export default {
   title: 'Game/GameDetails',
@@ -8,11 +8,22 @@ export default {
     backgrounds: {
       default: 'won-dark'
     }
+  },
+  args: {
+    platforms: ['linux', 'mac', 'windows']
+  },
+  argTypes: {
+    platforms: {
+      control: {
+        type: 'inline-check',
+        options: ['linux', 'mac', 'windows']
+      }
+    }
   }
 } as Meta
 
-export const Default: Story = () => (
+export const Default: Story<GameDetailsProps> = (args) => (
   <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
-    <GameDetails />
+    <GameDetails {...args} />
   </div>
 )
