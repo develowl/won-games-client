a883265f5ccf96e6aa205b2056a31eff77929dd7
Yan Gonçalves <fgyan94@gmail.com>
2022-10-23 17:06:03 -0300

    298. Cart Page - Parte 2: Criando o estado de empty e testes

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,6 +1,7 @@
 import CartList, { CartListProps } from 'components/CartList'
 import { Container } from 'components/Container'
 import { Divider } from 'components/Divider'
+import Empty from 'components/Empty'
 import { GameCardProps } from 'components/GameCard'
 import Heading from 'components/Heading'
 import { HighlightProps } from 'components/Highlight'
@@ -31,10 +32,22 @@ const Cart = ({
         <Heading lineLeft lineColor={'secondary'}>
           My cart
         </Heading>
-        <S.Content>
-          <CartList items={items} total={total} />
-          <PaymentOptions handlePayment={handlePayment} cards={cards} />
-        </S.Content>
+
+        {items.length ? (
+          <S.Content>
+            <CartList items={items} total={total} />
+            <PaymentOptions handlePayment={handlePayment} cards={cards} />
+          </S.Content>
+        ) : (
+          <Empty
+            title={'Your cart is empty'}
+            description={
+              'Go back to the store and explore great games and offers'
+            }
+            hasLink
+          />
+        )}
+
         <Divider />
 
         <Showcase
--- src/templates/Cart/test.tsx
+++ src/templates/Cart/test.tsx
@@ -1,14 +1,73 @@
 import { screen } from '@testing-library/react'
+import 'match-media-mock'
 import { renderWithTheme } from 'utils/tests/helpers'
+
+import itemsMock from 'components/CartList/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+import cardsMock from 'components/PaymentOptions/mock'
 
 import Cart from '.'
 
+const props = {
+  items: itemsMock,
+  total: 'R$ 460,00',
+  cards: cardsMock,
+  recommendedHighlight: highlightMock,
+  recommendedGames: gamesMock
+}
+
+jest.mock('templates/Base', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid={'Mock Base'}>{children}</div>
+  }
+}))
+
+jest.mock('components/Showcase', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid={'Mock Showcase'}>{children}</div>
+  }
+}))
+
+jest.mock('components/CartList', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid={'Mock CartList'}>{children}</div>
+  }
+}))
+
+jest.mock('components/PaymentOptions', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid={'Mock PaymentOptions'}>{children}</div>
+  }
+}))
+
+jest.mock('components/Empty', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid={'Mock Empty'}>{children}</div>
+  }
+}))
+
 describe('<Cart />', () => {
-  it('should render the heading', () => {
-    const { container } = renderWithTheme(<Cart />)
+  it('should render sections', () => {
+    renderWithTheme(<Cart {...props} />)
 
-    expect(screen.getByRole('heading', { name: /Cart/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /my cart/i })
+    ).toBeInTheDocument()
+    expect(screen.getByTestId('Mock CartList')).toBeInTheDocument()
+    expect(screen.getByTestId('Mock PaymentOptions')).toBeInTheDocument()
+    expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
+    expect(screen.queryByTestId('Mock Empty')).not.toBeInTheDocument()
+  })
 
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render empty section if there are no items', () => {
+    renderWithTheme(<Cart {...props} items={[]} />)
+
+    expect(screen.getByTestId('Mock Empty')).toBeInTheDocument()
   })
 })
