f3212e5fc75867113f700481e4e3ca23cf8f1204
Yan Gonçalves <fgyan94@gmail.com>
2022-11-09 23:00:23 -0300

    315. ExploreSidebar Parte 4: Criando método de filter

--- src/components/ExploreSidebar/styles.ts
+++ src/components/ExploreSidebar/styles.ts
@@ -3,5 +3,6 @@ import styled, { css } from 'styled-components'
 export const Wrapper = styled.div`
   /* ${({ theme }) => css`
     background-color: ${theme.colors.black};
+    color: ${theme.colors.black};
   `} */
 `
